8e4ea85531c8245d28f284975b32acd518dcca4a
Bruno Freitas <brunofreitas7@gmail.com>
2017-12-12 11:42:23 -0300

    debbug para analisar o  comportamento do travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,33 +2,40 @@
 echo "Starting deployment"
 echo "Target: gh-pages branch"
 
-DIST_DIRECTORY="dist/"
-CURRENT_COMMIT=`git rev-parse HEAD`
-ORIGIN_URL=`git config --get remote.origin.url`
-ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
+pwd
+ls
 
-cp .gitignore $DIST_DIRECTORY || exit 1
+# checkout da branch gh-pages to directory gh-pages
+# copy dist content to gh-pages
+# commit & push
 
-echo "Checking out gh-pages branch"
-git checkout -B gh-pages || exit 1
+#DIST_DIRECTORY="dist/"
+#CURRENT_COMMIT=`git rev-parse HEAD`
+#ORIGIN_URL=`git config --get remote.origin.url`
+#ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github.com}
 
-echo "Removing old static content"
-git rm -rf . || exit 1
+#cp .gitignore $DIST_DIRECTORY || exit 1
 
-echo "Copying dist content to root"
-cp -r $DIST_DIRECTORY/* . || exit 1
-cp $DIST_DIRECTORY/.gitignore . || exit 1
+#echo "Checking out gh-pages branch"
+#git checkout -B gh-pages || exit 1
 
-echo "Pushing new content to $ORIGIN_URL"
-git config user.name "$GH_USER" || exit 1
-git config user.email "$GH_USER_EMAIL" || exit 1
+#echo "Removing old static content"
+#git rm -rf . || exit 1
 
-git add -A . || exit 1
-git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
-git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
+#echo "Copying dist content to root"
+#cp -r $DIST_DIRECTORY/* . || exit 1
+#cp $DIST_DIRECTORY/.gitignore . || exit 1
 
-echo "Cleaning up temp files"
-rm -Rf $DIST_DIRECTORY
+#echo "Pushing new content to $ORIGIN_URL"
+#git config user.name "$GH_USER" || exit 1
+#git config user.email "$GH_USER_EMAIL" || exit 1
 
-echo "Deployed successfully."
+#git add -A . || exit 1
+#git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
+#git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
+
+#echo "Cleaning up temp files"
+#rm -Rf $DIST_DIRECTORY
+
+#echo "Deployed successfully."
 exit 0
