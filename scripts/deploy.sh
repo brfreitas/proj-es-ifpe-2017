d89c237151999ce8a3bbc721ca73799a1bfb0fcb
Bruno Freitas <brunofreitas7@gmail.com>
2017-12-12 14:14:19 -0300

    correção na referência do index.html

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,39 +16,9 @@ rm -rf .gitignore
 rm -rf .postcssrc.js
 rm -rf .travis.yml
 cp -a ../dist/. .
+sed -i -e 's/href=\//href=/g' -e 's/src=\//src=/g' index.html
 git add .
 git commit -m "Regenerated static content for $CURRENT_COMMIT"
 git push
 echo "Deployed successfully."
-# copy dist content to gh-pages
-# commit & push
-
-#DIST_DIRECTORY="dist/"
-#CURRENT_COMMIT=`git rev-parse HEAD`
-#
-
-#cp .gitignore $DIST_DIRECTORY || exit 1
-
-#echo "Checking out gh-pages branch"
-#git checkout -B gh-pages || exit 1
-
-#echo "Removing old static content"
-#git rm -rf . || exit 1
-
-#echo "Copying dist content to root"
-#cp -r $DIST_DIRECTORY/* . || exit 1
-#cp $DIST_DIRECTORY/.gitignore . || exit 1
-
-#echo "Pushing new content to $ORIGIN_URL"
-#git config user.name "$GH_USER" || exit 1
-#git config user.email "$GH_USER_EMAIL" || exit 1
-
-#git add -A . || exit 1
-#git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || exit 1
-#git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
-
-#echo "Cleaning up temp files"
-#rm -Rf $DIST_DIRECTORY
-
-#
 exit 0
