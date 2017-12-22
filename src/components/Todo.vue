d884defb1e48ce0cfebea498ba2052c23bda0e22
Bruno Freitas <brunofreitas7@gmail.com>
2017-12-22 10:28:15 -0300

    quebrando a build

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -2,7 +2,7 @@
   <div class="">
     <section class="todoapp">
       <header class="header">
-        <h1>todos</h1>
+        <h1>TODO</h1>
         <input class="new-todo"
         autofocus autocomplete="off"
         placeholder="What needs to be done?"
